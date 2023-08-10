716e5fff6f99ecc32cbb8b4a9ec4c512e41ccd1e
Hernan J <castro.hernan5@gmail.com>
2023-08-10 18:31:35 -0300

    NEW VERSION 3

--- components/ModalRoom.jsx
+++ components/ModalRoom.jsx
@@ -5,16 +5,15 @@ import { Fade } from 'react-slideshow-image';
 const ModalRoom = ({ show, instruments }) => (
   <div className="absolute bg-white top-0 left-0 right-0 bottom-0 h-screen sm:h-4/5 mt-10 p-4 m-4 z-10 sm:w-1/2 sm:mx-auto flex flex-col justify-around rounded-[24px] bg-indigo-200">
     <Fade duration={10000}>
-      {instruments.inst.map((inst, i) => {
-        return (
+      {instruments.inst.map((inst, i) (
           <div key={i} className="each-slide">
             <div className="mb-5 mx-auto">
               <img src={inst.img} className="h-80" />
             </div>
             <p className="p-4 text-center">{inst.desc}</p>
           </div>
-        );
-      })}
+        )
+      )}
     </Fade>
     <div className="mx-auto flex flex-col items-center gap-y-8">
       <h2 className="font-bold">{instruments.title}</h2>
--- components/WpButton.jsx
+++ components/WpButton.jsx
@@ -6,12 +6,12 @@ const WpButton = () => (
       href="https://api.whatsapp.com/send/?phone=5492235815178"
       target="_blank"
       className="fixed bottom-10 right-10 w-20 sm:w-24 sm:bottom-20 sm:right-20 z-50"
+      rel="noreferrer"
     >
       <img
         key="whatsApp"
         src="./wpicon.png"
         alt="whatsApp"
-        rel="noreferrer"
         className="cursor-pointer"
       />
     </a>
