7db0f55ad0de221130bfa520eb068b5f2d5df61f
Hernan J <castro.hernan5@gmail.com>
2023-08-28 20:29:48 -0300

    NEW VERSION 8

--- components/CalendarTurns.jsx
+++ components/CalendarTurns.jsx
@@ -1,9 +1,10 @@
 'use client';
+
+import { useEffect, useState } from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { format, parse, startOfWeek, getDay } from 'date-fns';
 import esES from 'date-fns/locale/es';
-import { useEffect, useState } from 'react';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const locales = {
   es: esES,
@@ -48,9 +49,9 @@ const CalendarTurns = ({ id }) => {
       body: JSON.stringify({ id }),
     })
       .then((res) => res.json())
-      .then((data) => {
-        data.map((turn) => {
-          return (events = [
+      .then((fetchData) => {
+        fetchData.map((turn) => {
+          (events = [
             ...events,
             {
               ...turn,
@@ -62,6 +63,7 @@ const CalendarTurns = ({ id }) => {
       });
     setTurns(events);
     setData(true);
+    return;
   };
   useEffect(() => {
     server();
@@ -69,25 +71,25 @@ const CalendarTurns = ({ id }) => {
   return !data ? (
     <div className="w-full h-[600px] flex justify-items-center items-center">
       <div className="sk-chase m-auto">
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
       </div>
     </div>
   ) : (
     <div>
       <Calendar
-        culture='es'
+        culture="es"
         localizer={localizer}
-        defaultView='work_week'
-        views={['day', 'work_week']}
+        defaultView="work_week"
+        views={["day", "work_week"]}
         events={turns}
-        startAccessor='start'
+        startAccessor="start"
         // subtract one millisecond from the end date putting midnight dates at 11:59:999
-        endAccessor={({ end }) => end.getHours() == 0 && end.getMinutes() == 0 ? new Date(end.getTime() - 1) : end}
+        endAccessor={({ end }) => end.getHours() === 0 && end.getMinutes() === 0 ? new Date(end.getTime() - 1) : end}
         showMultiDayTimes={true}
         min={new Date('2023-08-23T16:00:00')}
         style={{ height: 600 }}
