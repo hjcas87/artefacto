c9e784941ace7f98ae31efa617cacd899acc3667
Hernan J <castro.hernan5@gmail.com>
2023-09-05 22:00:45 -0300

    NEW VERSION 10

--- components/ModalRoom.jsx
+++ components/ModalRoom.jsx
@@ -2,6 +2,25 @@
 
 import Image from 'next/image';
 import { Fade } from 'react-slideshow-image';
+import Spinner from './Spinner';
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 const ModalRoom = ({ show, instruments }) => (
   // return instruments.inst && (
@@ -26,8 +45,9 @@ const ModalRoom = ({ show, instruments }) => (
               width={320}
               height={320}
               // loader={'Cargando'}
+              placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(320, 320))}`}
               loading="lazy"
-              placeholder="empty"
+              // placeholder="blur"
             />
             {/* <img
               src={inst.img}
